2f56495652c343b44c0afec6c45be5dbfe55a9fe
Ava Howell <ava@avahowell.me>
2023-09-15 05:40:11 -0700

    implement do_send_messages_and_wait_commit, working create_client

--- crates/relayer/src/chain/penumbra/endpoint.rs
+++ crates/relayer/src/chain/penumbra/endpoint.rs
@@ -1,12 +1,18 @@
 use bytes::{Buf, Bytes};
 use ibc_relayer_types::signer::Signer;
+use penumbra_proto::core::ibc::v1alpha1::IbcAction;
+use prost::Message;
+use rand::Rng;
 use std::time::Duration;
 use std::{str::FromStr, sync::Arc};
 
 use crate::chain::client::ClientSettings;
 use crate::chain::cosmos::query::status::query_status;
 use crate::chain::cosmos::query::QueryResponse;
+use crate::chain::cosmos::types::tx::{TxStatus, TxSyncResult};
+use crate::chain::cosmos::wait::wait_for_block_commits;
 use crate::chain::endpoint::{ChainEndpoint, ChainStatus, HealthCheck};
+use tendermint_rpc::endpoint::broadcast::tx_sync::Response;
 
 use crate::chain::requests::{IncludeProof, QueryHeight};
 use crate::chain::tracking::TrackedMsgs;
@@ -129,6 +135,36 @@ pub struct PenumbraChain {
 }
 
 impl PenumbraChain {
+    async fn get_anchor(
+        &self,
+    ) -> Result<penumbra_proto::core::crypto::v1alpha1::MerkleRoot, Error> {
+        let status = self
+            .rpc_client
+            .status()
+            .await
+            .map_err(|e| Error::rpc(self.config.rpc_addr.clone(), e))?;
+
+        let max_height = status.sync_info.latest_block_height;
+
+        let height = rand::thread_rng().gen_range(1 as u64..max_height.into());
+
+        let path = format!("sct/anchor/{}", height);
+
+        let res = self
+            .rpc_client
+            .abci_query(
+                Some("state/key".to_string()),
+                path.into_bytes(),
+                Some(max_height),
+                false,
+            )
+            .await
+            .map_err(|e| Error::rpc(self.config.rpc_addr.clone(), e))?;
+
+        Ok(penumbra_proto::core::crypto::v1alpha1::MerkleRoot {
+            inner: res.value[2..].into(),
+        })
+    }
     #[instrument(
         name = "send_messages_and_wait_commit",
         level = "error",
@@ -149,12 +185,115 @@ impl PenumbraChain {
             }
         );
 
+        let status = self
+            .rpc_client
+            .status()
+            .await
+            .map_err(|e| Error::rpc(self.config.rpc_addr.clone(), e))?;
+        let last_anchor = self.get_anchor().await?;
+
         let proto_msgs = tracked_msgs.msgs;
 
-        // TODO:
-        // pack proto_msgs into a penumbra tx, each proto_msg being an IbcAction
-        // send the tx to the penumbra chain
-        // poll the penumbra chain until the tx is committed
+        let mut ibc_actions = vec![];
+
+        for msg in proto_msgs {
+            //cursed
+            ibc_actions.push(penumbra_proto::core::transaction::v1alpha1::Action {
+                action: Some(
+                    penumbra_proto::core::transaction::v1alpha1::action::Action::IbcAction(
+                        IbcAction {
+                            raw_action: Some(pbjson_types::Any {
+                                type_url: msg.type_url,
+                                value: msg.value.into(),
+                            }),
+                        },
+                    ),
+                ),
+            });
+        }
+
+        let tx_body = penumbra_proto::core::transaction::v1alpha1::TransactionBody {
+            actions: ibc_actions.clone(),
+            fee: Some(penumbra_proto::core::crypto::v1alpha1::Fee {
+                amount: Some(penumbra_proto::core::crypto::v1alpha1::Amount { lo: 0, hi: 0 }),
+                asset_id: None,
+            }),
+            memo_data: Some(penumbra_proto::core::transaction::v1alpha1::MemoData {
+                encrypted_memo: vec![].into(),
+            }),
+            transaction_parameters: Some(
+                penumbra_proto::core::transaction::v1alpha1::TransactionParameters {
+                    expiry_height: 0,
+                    chain_id: "".to_string(),
+                },
+            ),
+            detection_data: None,
+        };
+
+        let tx = penumbra_proto::core::transaction::v1alpha1::Transaction {
+            body: Some(tx_body),
+            binding_sig: vec![0; 64].into(), // cool signature
+            anchor: Some(last_anchor),
+        };
+
+        let tx_bytes = tx.encode_to_vec();
+
+        // submit tx
+
+        let res = self
+            .rpc_client
+            .broadcast_tx_sync(tx_bytes)
+            .await
+            .map_err(|e| Error::rpc(self.config.rpc_addr.clone(), e))?;
+
+        let txsync_res = response_to_tx_sync_result(&self.config.id, ibc_actions.len(), res);
+
+        let mut txsync_responses = vec![txsync_res];
+
+        wait_for_block_commits(
+            &self.config.id,
+            &self.rpc_client,
+            &self.config.rpc_addr,
+            &self.config.rpc_timeout,
+            &mut txsync_responses,
+        )
+        .await?;
+
+        let events = txsync_responses
+            .into_iter()
+            .flat_map(|el| el.events)
+            .collect();
+
+        Ok(events)
+    }
+}
+
+fn response_to_tx_sync_result(
+    chain_id: &ChainId,
+    message_count: usize,
+    response: Response,
+) -> TxSyncResult {
+    if response.code.is_err() {
+        // Note: we don't have any height information in this case. This hack will fix itself
+        // once we remove the `ChainError` event (which is not actually an event)
+        let height = ibc_relayer_types::Height::new(chain_id.version(), 1).unwrap();
+
+        let events_per_tx = vec![IbcEventWithHeight::new(ibc_relayer_types::events::IbcEvent::ChainError(format!(
+            "check_tx (broadcast_tx_sync) on chain {} for Tx hash {} reports error: code={:?}, log={:?}",
+            chain_id, response.hash, response.code, response.log
+        )), height); message_count];
+
+        TxSyncResult {
+            response,
+            events: events_per_tx,
+            status: TxStatus::ReceivedResponse,
+        }
+    } else {
+        TxSyncResult {
+            response,
+            events: Vec::new(),
+            status: TxStatus::Pending { message_count },
+        }
     }
 }
 
