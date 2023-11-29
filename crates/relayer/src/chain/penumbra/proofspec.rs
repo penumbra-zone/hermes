d9193310f334e4571ade3beaf2d36e9442b5fe1d
Ava Howell <ava@avahowell.me>
2023-11-29 10:47:51 -0500

    penumbra: update proof spec to new multistore format

--- crates/relayer/src/chain/penumbra/proofspec.rs
+++ crates/relayer/src/chain/penumbra/proofspec.rs
@@ -31,40 +31,11 @@ fn jmt_spec_with_prehash() -> ics23::ProofSpec {
     spec
 }
 
-/// this is a proof spec for computing Penumbra's AppHash, which is defined as
-/// SHA256("PenumbraAppHash" || jmt.root()). In ICS/IBC terms, this applies a single global prefix
-/// to Penumbra's state. Having a stable merkle prefix is currently required for our IBC
-/// counterparties to verify our proofs.
-fn apphash_spec() -> ibc_proto::cosmos::ics23::v1::ProofSpec {
-    ics23::ProofSpec {
-        // the leaf hash is simply H(key || value)
-        leaf_spec: Some(ics23::LeafOp {
-            prefix: vec![],
-            hash: ics23::HashOp::Sha256.into(),
-            length: ics23::LengthOp::NoPrefix.into(),
-            prehash_key: ics23::HashOp::NoHash.into(),
-            prehash_value: ics23::HashOp::NoHash.into(),
-        }),
-        // NOTE: we don't actually use any InnerOps.
-        inner_spec: Some(ics23::InnerSpec {
-            hash: ics23::HashOp::Sha256.into(),
-            child_order: vec![0, 1],
-            child_size: 32,
-            empty_child: vec![],
-            min_prefix_length: 0,
-            max_prefix_length: 0,
-        }),
-        min_depth: 0,
-        max_depth: 1,
-        prehash_key_before_comparison: false,
-    }
-}
-
 // TODO: this should re-export the proof specs from the Penumbra crate
 pub fn penumbra_proof_spec_no_prehash() -> ProofSpecs {
-    vec![jmt_spec_no_prehash(), apphash_spec()].into()
+    vec![jmt_spec_no_prehash(), jmt_spec_no_prehash()].into()
 }
 
 pub fn penumbra_proof_spec_with_prehash() -> ProofSpecs {
-    vec![jmt_spec_with_prehash(), apphash_spec()].into()
+    vec![jmt_spec_with_prehash(), jmt_spec_with_prehash()].into()
 }
