fe3e18081b6b7199710976b709d4078a91ed2dc8
Ava Howell <ava@avahowell.me>
2024-02-01 13:07:11 -0800

    implement the majority of query and event parsing

--- crates/relayer/src/chain/cosmos.rs
+++ crates/relayer/src/chain/cosmos.rs
@@ -2380,7 +2380,7 @@ impl ChainEndpoint for CosmosSdkChain {
     }
 }
 
-fn sort_events_by_sequence(events: &mut [IbcEventWithHeight]) {
+pub fn sort_events_by_sequence(events: &mut [IbcEventWithHeight]) {
     events.sort_by(|a, b| {
         a.event
             .packet()
